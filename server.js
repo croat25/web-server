0cad821bbced260af178755a26a99e1e665d18da
Bruno Salapic <sala4460@mylaurier.ca>
2016-12-19 14:12:10 -0500

    Initiliza repo

--- server.js
+++ server.js
@@ -18,10 +18,11 @@ app.use(middleware.logger);
 //app.use(middleware.requireAuthentication);
 app.get('/about',middleware.requireAuthentication,function(req,res){
 
-	res.send("about us!");
+	res.send("about us!FUCK YOU");
 });
 
 app.use(express.static(__dirname+"/public"));
 app.listen(port,function(){
 	console.log("server started and port : "+port);
+
 });
